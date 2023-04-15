d22ae6c294d4bc9de574710a748e117b6e5a50b0
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-04-15 13:54:45 -0700

    lab done

--- js/index.js
+++ js/index.js
@@ -1,69 +1,69 @@
 // Iteration 1: Names and Input
-//
-const hacker1 = 'dominic';
+const hacker1 = "dom"
 console.log(`The driver's name is ${hacker1}`)
 
-const hacker2 = 'guillermo';
+const hacker2 = "tony"
 console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
-} else if (hacker2.length > hacker1.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}else if (hacker1.length < hacker2.length) {
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters.`)
 } else {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+  console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
 }
 
+console.log("-------------")
 // Iteration 3: Loops
 
-let newWord = hacker1.toUpperCase();
- const newArr = newWord.split('')
-    console.log(newArr.join(" "))
+ console.log(hacker1.toUpperCase().split("").join(" "))
 
-    // reverse word 
-    let reverseWord = hacker1.split("").reverse().join("");
+// reverse word 
 
-    console.log(reverseWord)
+console.log(hacker1.toUpperCase().split("").reverse().join(" "))
 
 
-    // alphabetic order
+// alphabetic order
 
-    if (hacker1 > hacker2) {
-        console.log("The driver's name goes first.")
-      }
-      else if (hacker1 < hacker2) {
-        console.log("Yo, the navigator goes first definitely.")
-      }
-      else {
-        console.log("What?! You both have the same name?")
-      } 
-      
-      // lorem ipsum generator
+if (hacker1 > hacker2) {
+  console.log(`The driver's name goes first.`)
+}else if (hacker1 < hacker2) {
+    console.log(`Yo, the navigator goes first definitely.`)
+} else {
+  console.log(`What?! You both have the same name?`)
+}
 
-      const paragraphGenerator = ((word) => {
-        const numberOfWords = word.split(" ").length
-        
-        const count = word.split('et').length - 1;
-        return `The paragraph has ${numberOfWords} & the letters "et" has repeteated ${count} many times.`
-      });
-   
-      console.log(paragraphGenerator(paragraph))
+ // lorem ipsum generator
 
-      // palindrome checker
+const par = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
 
-      const palindromeChecker = ((phrase) => {
-        // get unwanted characters ","
-        const unwanted = /[\W_]/g;
-        // lower case everything and replace the unwanted
-        let lowerString = phrase.toLowerCase().replace(unwanted, "");
-        // chain the string and reverse 
-        let reverseString = lowerString.split("").reverse().join("")
-        // verify is the lowerString is equal to reverseString
-        return reverseString === lowerString
-      });
+console.log(`The paragraph is ${par.split(" ").length} words long.`)
+
+let counter = 0
+const arr = par.split(" ")
+
+arr.forEach((word) => {
+  if (word.includes('et')) {
+    counter += 1
+  }
+});
+
+console.log(`The paragraph has ${counter} times the word "et".`)
+
+// palindrome checker
+
+let phraseToCheck = "race car"
+
+let wordWithoutSpace = phraseToCheck.split(" ").join("")
+
+let wordWithoutSpaceReverse = phraseToCheck.split("").reverse().join("").split(" ").join("")
+
+console.log(wordWithoutSpace)
+
+console.log(wordWithoutSpaceReverse === wordWithoutSpaceReverse)
    
     
-    console.log(palindromeChecker("Amor, Roma"));
+
 
